31d505109c16e7ad6f5a2281a745b1b476c0bc5e
Mohamed Nisfan <mohnisfan@gmail.com>
2024-12-06 08:21:10 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+app.use(logger());
+
+app.get("/", async (c) => {
+  await new Promise((resolve) => setTimeout(() => resolve(0), 0));
+  return c.text("Hello Hono!");
+});
+
+export default app;
